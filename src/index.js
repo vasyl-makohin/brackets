0a006a1863c18ccd9c500d1f5d0f49068858e08f
Vasyl M <makohin.v@gmail.com>
2022-02-07 23:07:47 +0200

    feat: add function brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 == 1) {
+    return false;
+  }
+  let stack = new Array();
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    let find = false;
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] == bracketsConfig[j][0]) {
+        find = true;
+
+        if (bracketsConfig[j][0] == bracketsConfig[j][1]) {
+          if (stack[stack.length - 1] == bracketsConfig[j][0]) {
+            stack.pop();
+            break;
+          }
+        }
+
+        stack.push(bracketsConfig[j][1]);
+        break;
+      }
+    }
+
+    if (!find) {
+      if (stack.length == 0 || str[i] != stack[stack.length - 1]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length == 0 ? true : false;
+};
